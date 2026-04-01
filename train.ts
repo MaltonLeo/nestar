07e9b01eff4c96fe10af2d0ebf91def42fbfabb4
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-02 01:43:27 +0900

    Task ZK

--- train.ts
+++ train.ts
@@ -1,24 +1,45 @@
-// TASK ZJ:
+// TASK-ZK:
 
-// Shunday function yozing, u berilgan array ichidagi
-// raqamlarni qiymatini hisoblab qaytarsin.
+// Shunday function yozing, u har soniyada
+//  bir marta consolega 1 dan 5 gacha bolgan
+//   raqamlarni chop etsin va 5 soniyadan
+//    keyin ishini toxtatsin.
+// MASALAN: printNumbers()
 
-// MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
+function printNumbers() {
+  let i = 1;
 
-// Yuqoridagi misolda, array nested bo'lgan holdatda ham,
-// bizning function ularning yig'indisini hisoblab qaytarmoqda.
-
-function reduceNestedArray(arr) {
-  let sum = 0;
-
-  for (let item of arr) {
-    if (typeof item === 'number') {
-      sum += item;
-    } else if (Array.isArray(item)) {
-      sum += reduceNestedArray(item)
+  const interval = setInterval(() => {
+    console.log(i)
+    i++
+     if (i > 5) {
+      clearInterval(interval);
     }
-  }
-
-  return sum;
+  }, 1000);
 }
-console.log(reduceNestedArray([1,2,3,[1,3,8]]))
+printNumbers()
+
+// // TASK ZJ:
+
+// // Shunday function yozing, u berilgan array ichidagi
+// // raqamlarni qiymatini hisoblab qaytarsin.
+
+// // MASALAN: reduceNestedArray([1, [1, 2, [4]]]); return 8;
+
+// // Yuqoridagi misolda, array nested bo'lgan holdatda ham,
+// // bizning function ularning yig'indisini hisoblab qaytarmoqda.
+
+// function reduceNestedArray(arr) {
+//   let sum = 0;
+
+//   for (let item of arr) {
+//     if (typeof item === 'number') {
+//       sum += item;
+//     } else if (Array.isArray(item)) {
+//       sum += reduceNestedArray(item)
+//     }
+//   }
+
+//   return sum;
+// }
+// console.log(reduceNestedArray([1,2,3,[1,3,8]]))
