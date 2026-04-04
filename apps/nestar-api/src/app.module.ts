d43ec0a85fbf1d65e2f63dc499d202137a2baced
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-04 12:22:04 +0900

    feat: integrate validation pipes logic

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -7,7 +7,7 @@ import {ApolloDriver} from "@nestjs/apollo"
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
-console.log("MONGO ENV:", process.env.MONGO_DEV);
+
 @Module({
   imports: [
     ConfigModule.forRoot(), GraphQLModule.forRoot({
