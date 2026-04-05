0b3a0c3f82afd5f1c0a87e65c47103b486572294
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-06 08:19:15 +0900

    feat: develop logging via interceptors

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -1,16 +1,40 @@
-import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
 import { Observable, tap } from "rxjs";
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor{
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-        console.log("Before ...")
+    private readonly logger: Logger = new Logger();
 
-        const now = Date.now();
-        return next
+    public intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+        const recordTime = Date.now();   
+        const requestType = context.getType<GqlContextType>();  
+       
+    
+        if(requestType === "http") {
+        return next.handle();
+         //**Develop if needed */
+         } else if(requestType === "graphql"){
+            //**(1) Develop if needed */
+            const gqlContext = GqlExecutionContext.create(context)
+            this.logger.log(` ${this.stringify(gqlContext.getContext().req.body)}`, "REQUEST")
+           
+            //**Errors handling via GraphQL */
+
+            //**No Errors giving Response below */
+         return next
         .handle()
         .pipe(
-            tap(() => console.log(`After ... ${Date.now() - now}`))
+            tap((context) =>{
+                const responseTime = Date.now() - recordTime;
+                this.logger.log(`${this.stringify(context)} - ${responseTime}ms \n\n`, "RESPONSE") 
+        })
         )
+        }
+    
+       return next.handle();  
     }
+      private stringify(context : ExecutionContext):string{
+        return JSON.stringify(context).slice(0,75)
+      }
 }
