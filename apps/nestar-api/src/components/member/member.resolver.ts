5d76a059ececfcd3401afba0701b2257d2abb602
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-09 01:25:55 +0900

    feat : integration of Authentication and Authorization

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,11 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException } from '@nestjs/common';
+import { InternalServerErrorException, UseGuards } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import type { ObjectId } from 'mongoose';
 
 
 @Resolver()
@@ -23,10 +26,20 @@ export class MemberResolver {
     }
 
     //Authenticated
+    @UseGuards(AuthGuard)
     @Mutation(() => String)
-    public async updateMember(): Promise<string> {
+    public async updateMember(@AuthMember("_id") memberId: ObjectId): Promise<string> {
         console.log("Mutation: updateMember");
         return this.memberService.updateMember();
+    }
+
+     @UseGuards(AuthGuard)
+    @Mutation(() => String)
+    public async checkAuth(@AuthMember("memberNick") memberNick: string): Promise<string> {
+        console.log("Query: checkAuth");
+        console.log("memberNick:", memberNick)
+        return `Hi ${memberNick}`;
+        
     }
 
     @Query(() => String)
@@ -37,13 +50,13 @@ export class MemberResolver {
 
     //**ADMIN */
 
-//Authorizarion
+//Authorizarion ADMIN
     @Mutation(()=> String)
     public async getAllMemberByAdmmin(): Promise<string>{
      return this.memberService.getAllMemberByAdmin();
     }
 
-    //Authorizarion
+    //Authorizarion ADMIN
     @Mutation(()=> String)
     public async updateMemberByAdmmin(): Promise<string>{
     console.log('Mutation : updateMemberByAdmin') 
