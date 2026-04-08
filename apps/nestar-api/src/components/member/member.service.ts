1940b49287ccb497145f4cd82f5841f3ffd67471
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-08 22:17:23 +0900

    feat: hashing password via bcryptjs

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -5,18 +5,21 @@ import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
-    constructor(@InjectModel("Member") private readonly memberModel:Model<Member>){}
+    constructor(@InjectModel("Member") private readonly memberModel:Model<Member>,
+     private authService: AuthService){}
 
     public async signup(input:MemberInput): Promise<Member>{
-        //TODO Hash password
+        input.memberPassword = await  this.authService.hashPassword(input.memberPassword)
         try {
             const result = await this.memberModel.create(input);
             //TODO Authentication via Token
             return result
         } catch (err) {
+            if(err instanceof Error)
             console.log("Error, Service.model:", err.message)
             throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
         }
@@ -38,7 +41,7 @@ export class MemberService {
 
        //// TODO Compare passwords
       
-       const isMatch = memberPassword === response.memberPassword;
+       const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword!);
        if(!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD)
 
         return response
