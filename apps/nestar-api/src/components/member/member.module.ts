b1d4cdfb87bd7e4bbab95ef6af237656e8789ad5
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-28 00:12:13 +0900

    fix: modify job scheduler business logic

--- apps/nestar-api/src/components/member/member.module.ts
+++ apps/nestar-api/src/components/member/member.module.ts
@@ -2,7 +2,7 @@ import { forwardRef, Module } from '@nestjs/common';
 import { MemberResolver } from './member.resolver';
 import { MemberService } from './member.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import MemberSchema from '../../schemas/Member_model';
+import MemberSchema from '../../schemas/Member.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { LikeModule } from '../like/like.module';
