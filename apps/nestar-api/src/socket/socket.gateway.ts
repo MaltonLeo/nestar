205b0e0365b94c3ec5c76f9b6e85298573d52be0
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-05-28 12:39:16 +0900

    fix:modify WebSocket chatting server final stage

--- apps/nestar-api/src/socket/socket.gateway.ts
+++ apps/nestar-api/src/socket/socket.gateway.ts
@@ -2,22 +2,32 @@ import { Logger } from '@nestjs/common';
 import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server } from 'ws';
 import * as WebSocket from 'ws';
+import { AuthService } from '../components/auth/auth.service';
+import * as url from 'url';
+import { Member } from '../libs/dto/member/member';
 
 interface MessagePayload {
   event: string;
   text: string;
+  memberData?: Member | null;
 }
 
 interface InfoPayload {
   event: string;
   totalClients: number;
+  memberData?: Member | null;
+  action: string;
 }
 
 @WebSocketGateway({transports: ['websocket'], secure: false})
 export class SocketGateway  implements OnGatewayInit{
   private logger: Logger = new Logger('SocketEventsGateway');
   private summaryClient : number = 0 ;
-  
+  private clientAuthMap = new Map <WebSocket, Member | null>()
+  private messageList: MessagePayload[] = [];
+
+constructor(private authService: AuthService) {}
+
    @WebSocketServer()
    server!: Server;
 
@@ -25,33 +35,64 @@ export class SocketGateway  implements OnGatewayInit{
       this.logger.verbose(`WebSocket Server Initialized total: [${this.summaryClient}]`)
    
     }
+
+    private async retrieveAuth(req: any): Promise<Member | null> {
+      try {
+        const parseUrl = url.parse(req.url, true);
+        const {token} = parseUrl.query;
+        console.log('token:', token);
+        return await this.authService.verifyToken(token as string)
+      } catch (error) {
+        console.log('retrieveAuth ERROR:', error);
+        return null
+      }
+    }
    
-   handleConnection(client : WebSocket, ...args: any[]) {
+  public async handleConnection(client : WebSocket, req: any) {
+    const authMember = await this.retrieveAuth(req);
     this.summaryClient++;
-    this.logger.verbose(`Connection total: [${this.summaryClient}]`);
+
+    this.clientAuthMap.set(client, authMember);
+    console.log('authMember:', authMember);
+
+    const clientNick: string = authMember?.memberNick ?? "Guest";
+    this.logger.verbose(`Connection[${clientNick}] total: [${this.summaryClient}]`);
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action: "joined",
     };
     this.emitMessage(infoMsg);
+    client.send(JSON.stringify({event: 'getMessages', list : this.messageList}))
   }
 
-   handleDisconnect(client: WebSocket) {
+  public handleDisconnect(client: WebSocket) {
+    const authMember = this.clientAuthMap.get(client);
     this.summaryClient--;
-    this.logger.log(`== Client disconnected left total: ${this.summaryClient} ==`);
+    this.clientAuthMap.delete(client);
+
+    const clientNick: string = authMember?.memberNick ?? "Guest";
+    this.logger.log(`Disconnection [${clientNick}] & total ${this.summaryClient}`);
       
     const infoMsg: InfoPayload = {
       event: 'info',
       totalClients: this.summaryClient,
+      memberData: authMember,
+      action:'left',
     }; 
    this.broadcastMessage(client,infoMsg);
   }
 
   @SubscribeMessage('message')
 public async handleMessage(client: WebSocket, payload: string): Promise<void> {
-  const newMessage: MessagePayload = { event: 'message', text: payload };
+  const authMember = this.clientAuthMap.get(client)
+  const newMessage: MessagePayload = { event: 'message', text: payload, memberData: authMember };
   
+const clientNick: string = authMember?.memberNick ?? "Guest";
   this.logger.verbose(`NEW MESSAGE: ${payload}`);
+  this.messageList.push(newMessage)
+  if(this.messageList.length > 5) this.messageList.splice(0, this.messageList.length - 5)
   this.emitMessage(newMessage);
 }
 
@@ -71,3 +112,9 @@ public async handleMessage(client: WebSocket, payload: string): Promise<void> {
     })
   }
 }
+/**
+ * Message Target:
+ * 1.Client-faqat client o'ziga
+ * 2.Broadcast - client dan boshqalarga
+ * 3.Emit (barcha clientlarga)
+ */
